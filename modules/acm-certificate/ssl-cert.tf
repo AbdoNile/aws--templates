c319efd27112370d0f916648b3b36b9c8747f6e8
Abdo <abdo@axetay.co.uk>
2019-07-13 21:53:23 +0100

    fix certificate subdomain

--- modules/acm-certificate/ssl-cert.tf
+++ modules/acm-certificate/ssl-cert.tf
@@ -5,13 +5,12 @@ variable "ssl-domain-root" {
 
 
 resource "aws_acm_certificate" "certificate" {
-  domain_name       = "*.${var.ssl-domain-root}"
+  domain_name       = "${var.ssl-domain-root}"
   validation_method = "DNS"
   subject_alternative_names = ["*.${var.ssl-domain-root}"]
-}
 
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = "${aws_acm_certificate.certificate.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
--- modules/acm-certificate/validation.tf
+++ modules/acm-certificate/validation.tf
@@ -3,6 +3,10 @@ data "aws_route53_zone" "domain-hosted-zone" {
   name = "${var.ssl-domain-root}"
 }
 
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.certificate.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
 
 resource "aws_route53_record" "cert_validation" {
   name    = "${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name}"
